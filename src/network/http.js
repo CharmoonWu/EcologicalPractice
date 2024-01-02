cd2655d380bfc02a568e75e606d591a3af34510c
charmoon <272043729@qq.com>
2024-01-02 18:14:41 +0800

    no message

--- src/network/http.js
+++ src/network/http.js
@@ -5,6 +5,7 @@ const envDev = '/api';
 
 const APPID = 'iqcrcsjrjrtvj0kn';
 const APPSECRET = 'OsYvCSUvUZPgyCGQsj8m8jBkkRqHd181';
+const Text = '';
 
 const instance = axios.create({
   baseURL: BASE_URL + envDev,
