38a5e84db118281de95d5dcb6410772929762c14
Charmoon Wu <charmoonwu@cherrypicks.com>
2024-01-02 20:53:11 +0800

    test+1

--- src/network/http.js
+++ src/network/http.js
@@ -5,7 +5,7 @@ const envDev = '/api';
 
 const APPID = 'iqcrcsjrjrtvj0kn';
 const APPSECRET = 'OsYvCSUvUZPgyCGQsj8m8jBkkRqHd181';
-const Text = 'test';
+const Text = 'test+1';
 
 const instance = axios.create({
   baseURL: BASE_URL + envDev,
